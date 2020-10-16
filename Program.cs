944a122457a64cc42bd663cbda0d081bd4ab4862
Pawel Lukasik <pawel.lukasik@makeitright.ia>
2020-10-16 12:09:36 +0200

    add robot

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace example_client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(args[0]);
+            int y = int.Parse(args[1]);
+
+            Console.WriteLine($"Multiply {x} and {y} result = {Multiply(x, y)}");
+        }
+
+        static int Multiply(int x, int y)
+        {
+            return x * y;
+        }
+    }
+}
